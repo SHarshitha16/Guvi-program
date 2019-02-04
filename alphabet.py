af3edf521c830edee3d58e09708b6de9548c1858
SHarshitha16 <47297658+SHarshitha16@users.noreply.github.com>
2019-02-04 10:23:49 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,6 +1,10 @@
 #Harshitha
-ch=input()
-if((ch='a' and ch='z')or(ch='A' and ch='Z')):
-    print("alphabet")                 
+print("Enter '0' for exit.");
+ch = input("Enter any character: ");
+if ch == '0':
+    exit();
 else:
-    print("no")
+    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    	print(ch, "is an alphabet.");
+    else:
+    	print(ch, "is not an alphabet.");
